8ccbe4ca4ddbe126d8b136a718d2cc687b7a4f58
InkyungBaek <nash4comp@gmail.com>
2022-05-24 12:08:40 -0700

    Fixed the display bug. Added contactNumber for sending message.

--- public/js/caller_profile_call_for_help.js
+++ public/js/caller_profile_call_for_help.js
@@ -1,21 +1,33 @@
 "use strict";
+
+let contactNumber;
 function submitCallForHelp() {
     console.log("submitted");
-    let contactNumber = "7786972351";
-    checkM(checkMobile(), contactNumber);
+    let number = contactNumber.substr(0,3) + contactNumber.substr(4,3) + contactNumber.substr(8,4);
+    console.log("Message to: " + number);
+    checkM(checkMobile(), number);
 }
 
 function prepareCallForHelpModal() {
     document.getElementById("previewContents").value = "";
     document.getElementById("textContents").value = "";
     document.getElementById("callForHelpPreviewButton").onclick = async function () {
+        let response = await getData("/getUser");
+        if (response) {
+            if (response.status == "fail") {
+                console.log(response.msg);
+            } else {
+                contactNumber = response.user.contactNumber;
+                document.getElementById("contactNumber").innerHTML = contactNumber; // TODO change this to registered number from database
+            }
+        }
         previewCallForHelp();
     }
 }
 
 function previewCallForHelp() {
     console.log("preview");
-    document.getElementById("previewContents").innerText = ``;
+    document.getElementById("previewContents").value = ``;
     getUserLocation();
 }
 
@@ -26,8 +38,8 @@ function updateText(la, lo) {
     let timeStamp = today.toLocaleString();
     const messageText = document.getElementById('textContents').value;
     let previewText = `Current time: ` + timeStamp + ` \n` + messageText + ` \n` + `https://maps.google.co.kr/?ll=` + latitude + `,` + longitude;
-
-    document.getElementById("previewContents").innerText = previewText;
+    console.log(previewText);
+    document.getElementById("previewContents").value = previewText;
 }
 
 function checkMobile() {
@@ -36,17 +48,14 @@ function checkMobile() {
     if (varUA.indexOf('android') > -1) {
         //For Android
         console.log("android");
-        document.getElementById("callForHelpStatus").innerHTML = "android";
         return "android";
     } else if (varUA.indexOf("iphone") > -1 || varUA.indexOf("ipad") > -1 || varUA.indexOf("ipod") > -1) {
         //For iOS
         console.log("ios");
-        document.getElementById("callForHelpStatus").innerHTML = "ios";
         return "ios";
     } else {
         //For other OS
         console.log("other");
-        document.getElementById("callForHelpStatus").innerHTML = "others";
         return "other";
     }
 }
@@ -73,6 +82,6 @@ function checkM(m, contactNumber) {
     const text = document.getElementById('previewContents').value;
     console.log(text);
     console.log(phoneNumber);
-    location.href = 'sms:' + phoneNumber + (m == 'ios' ? '&' : '?') + 'body=' + encodeURIComponent(text);
     console.log("message sent to " + phoneNumber);
+    location.href = 'sms:' + phoneNumber + (m == 'ios' ? '&' : '?') + 'body=' + encodeURIComponent(text);
 }
