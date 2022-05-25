95415750843444744f4681043c441425cff0e0b5
InkyungBaek <nash4comp@gmail.com>
2022-05-25 10:57:48 -0700

    Added comments

--- public/js/caller_profile_call_for_help.js
+++ public/js/caller_profile_call_for_help.js
@@ -1,8 +1,11 @@
 "use strict";
 
+// When the user clicks the submit button, it opens the text messaging application in user's device.
 function submitCallForHelp() {
     let contactNumber = document.getElementById("callForHelpNumberInput").value;
+    // Cleans the text input box of contact number.
     document.getElementById("callForHelpStatus").innerHTML = ``;
+    // Verifies the contact number is valid.
     if (contactNumber == `` || contactNumber.length != 10 || isNaN(contactNumber)) {
         document.getElementById("callForHelpStatus").innerHTML = "Please input valid phone number.";
     } else {
@@ -11,6 +14,7 @@ function submitCallForHelp() {
     }
 }
 
+// Initializes the call for help modal window.
 function prepareCallForHelpModal() {
     document.getElementById("callForHelpNumberInput").value = ``;
     document.getElementById("previewContents").value = ``;
@@ -20,11 +24,14 @@ function prepareCallForHelpModal() {
     };
 }
 
+// Generate text message based on the user input.
+// It adds time stamp, current user location, and Google link.
 function previewCallForHelp() {
     document.getElementById("previewContents").innerText = ``;
     getUserLocation();
 }
 
+// Updates the generated text message into preview text box.
 function updateText(la, lo) {
     let latitude = la;
     let longitude = lo;
@@ -35,20 +42,20 @@ function updateText(la, lo) {
     document.getElementById("previewContents").value = previewText;
 }
 
+// Detects the OS of user's device to figure out the method for calling text message application.
 function checkMobile() {
-    // Get userAgent
+    // Get userAgent information.
     var varUA = navigator.userAgent.toLowerCase();
     if (varUA.indexOf('android') > -1) {
         return "android";
     } else if (varUA.indexOf("iphone") > -1 || varUA.indexOf("ipad") > -1 || varUA.indexOf("ipod") > -1) {
-        //For iOS
         return "ios";
     } else {
-        //For other OS
         return "other";
     }
 }
 
+// Gets the current location information.
 function success({
     coords,
 }) {
@@ -57,6 +64,7 @@ function success({
     updateText(latitude, longitude);
 }
 
+// Checks the navigator to get the current location information.
 function getUserLocation() {
     if (!navigator.geolocation) {
         throw "Location information is not supported.";
@@ -64,9 +72,11 @@ function getUserLocation() {
     navigator.geolocation.getCurrentPosition(success);
 }
 
+// Opens the text messaging applicaion for each device 
 function checkM(m, contactNumber) {
     let phoneNumber = contactNumber;
     const text = document.getElementById('previewContents').value;
+    // If the preview box is empty, it shows the alert message to prevent unintended results. 
     if (text == ``) {
         document.getElementById("callForHelpStatus").innerHTML = "Please preview the text before submission.";
     } else {
